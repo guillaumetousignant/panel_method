9eb5e4cf0875a4846848cbd3711cf29cced0dd2d
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-01-29 08:30:18 -0500

    enough with the snake case warnings wow

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,37 @@ fn main() {
 }
 
 struct COF {
-    A: Vec<f64>,
-    B: Vec<f64>,
-    N: usize,
+    a: Vec<f64>,
+    b: Vec<f64>,
+    n: usize,
 }
 
-// Assumes A is of size n*n and B is of size n
+// Assumes a is of size n*n and b is of size n
 fn gauss(m: usize, cof: &mut COF) {
     println!("The value of m is: {}", m);
-    let n = cof.B.len();
+    let n = cof.b.len();
 
     for k in 1..n {
         let kp = k + 1;
         for i in kp..n+1 {
-            let r = cof.A[i*n + k]/cof.A[k*n + k];
+            let r = cof.a[i*n + k]/cof.a[k*n + k];
             for j in kp..n+1 {
-                cof.A[i*n + j] -= r*cof.A[k*n + j];
+                cof.a[i*n + j] -= r*cof.a[k*n + j];
             }
             for j in 1..m+1 {
-                cof.B[i*n + j] -= r*cof.B[k*n + j];
+                cof.b[i*n + j] -= r*cof.b[k*n + j];
             }
         }
     }
 
     for k in 1..m+1 {
-        cof.B[n*n + k] /= cof.A[n*n + n];
+        cof.b[n*n + k] /= cof.a[n*n + n];
         for i in (n-1..0).rev() {
             let ip = i + 1;
             for j in ip..n+1 {
-                cof.B[i*n + k] -= cof.A[i*n + j] * cof.B[j*n + k];
+                cof.b[i*n + k] -= cof.a[i*n + j] * cof.b[j*n + k];
             }
-            cof.B[i*n + k] /= cof.A[i*n + i];
+            cof.b[i*n + k] /= cof.a[i*n + i];
         }
     }
 
