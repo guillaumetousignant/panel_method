7055b91be34baf27d571790175115397485c3167
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-01-28 21:57:55 -0500

    uses struct now

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,38 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct COF {
+    A: Vec<f64>,
+    B: Vec<f64>,
+    N: usize,
+}
+
 // Assumes A is of size n*n and B is of size n
-fn gauss(M: usize, A: &mut Vec<f64>, B: &mut Vec<f64>, n: isize) {
-    println!("The value of M is: {}", M);
-    let n = B.len();
+fn gauss(m: usize, cof: &mut COF) {
+    println!("The value of m is: {}", m);
+    let n = cof.B.len();
 
     for k in 1..n {
         let kp = k + 1;
         for i in kp..n+1 {
-            let r = A[i*n + k]/A[k*n + k];
+            let r = cof.A[i*n + k]/cof.A[k*n + k];
             for j in kp..n+1 {
-                A[i*n + j] -= r*A[k*n + j];
+                cof.A[i*n + j] -= r*cof.A[k*n + j];
             }
-            for j in 1..M+1 {
-                B[i*n + j] -= r*B[k*n + j];
+            for j in 1..m+1 {
+                cof.B[i*n + j] -= r*cof.B[k*n + j];
             }
         }
     }
 
-    for k in 1..M+1 {
-        B[n*n + k] /= A[n*n + n];
+    for k in 1..m+1 {
+        cof.B[n*n + k] /= cof.A[n*n + n];
         for i in (n-1..0).rev() {
             let ip = i + 1;
             for j in ip..n+1 {
-                B[i*n + k] -= A[i*n + j] * B[j*n + k];
+                cof.B[i*n + k] -= cof.A[i*n + j] * cof.B[j*n + k];
             }
-            B[i*n + k] /= A[i*n + i];
+            cof.B[i*n + k] /= cof.A[i*n + i];
         }
     }
 
