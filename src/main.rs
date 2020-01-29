56ad3e0f61495358d5e87875aff90b27e0ecde93
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-01-29 12:04:23 -0500

    added vpdis

--- src/main.rs
+++ src/main.rs
@@ -18,36 +18,123 @@ struct COF {
     n: usize,
 }
 
-fn coef(sin_alpha: f64, cos_alpha: f64, bod: &mut BOD, cof: &mut COF) {
+struct CPD {
+    ue: Vec<f64>,
+    cp: Vec<f64>,
+}
 
+fn coef(sin_alpha: f64, cos_alpha: f64, bod: &mut BOD) -> COF{
+    let n = bod.ndtot + 1;
+    let mut a = vec![0.; n];
+    let mut bv = vec![0.; n];
+    for j in 0..n {
+        a[n*n + j] = 0.;
+    }
+    for i in 0..n {
+        a[i*n + n] = 0.;
+        for j in 0..n {
+            let (flog, ftan) = match j == i {
+                true => (0., std::f64::consts::PI),
+                false => {
+                    let dxj = bod.x_mid[i] - bod.x[j];
+                    let dxjp = bod.x_mid[i] - bod.x[j+1];
+                    let dyj = bod.y_mid[i] - bod.y[j];
+                    let dyjp = bod.y_mid[i] - bod.y[j+1];
+                    (0.5 * ((dxjp*dxjp + dyjp*dyjp)/(dxj*dxj + dyj*dyj)).ln(), (dyjp*dxj - dxjp*dyj).atan2(dxjp*dxj + dyjp*dyj))
+                },
+            };
+
+            let ctimtj = bod.costhe[i]*bod.costhe[j] + bod.sinthe[i]*bod.sinthe[j];
+            let stimtj  = bod.sinthe[i]*bod.costhe[j] - bod.costhe[i]*bod.sinthe[j];
+            a[i*n + j] = 0.5/std::f64::consts::PI * (ftan*ctimtj + flog*stimtj);
+            let b = 0.5/std::f64::consts::PI * (flog*ctimtj - ftan*stimtj);
+            a[i*n + n] += b;
+
+            if (i < 1) || (i >= n-1) {
+                a[i*n + j] -= b;
+                a[n*n + n] += a[i*n + j];
+            }   
+        }
+        bv[i] = bod.sinthe[i]*cos_alpha - bod.costhe[i]*sin_alpha;
+    }
+    bv[n] = -(bod.costhe[0] + bod.costhe[bod.ndtot])*cos_alpha - (bod.sinthe[0] + bod.sinthe[bod.ndtot])*sin_alpha;
+    
+    COF {
+        a, 
+        b: bv, 
+        n
+    }
 }   
 
 // Assumes a is of size n*n and b is of size n
 fn gauss(m: usize, cof: &mut COF) {
     let n = cof.b.len();
 
-    for k in 0..n {
+    for k in 0..n-1 {
         let kp = k + 1;
-        for i in kp..n+1 {
+        for i in kp..n {
             let r = cof.a[i*n + k]/cof.a[k*n + k];
-            for j in kp..n+1 {
+            for j in kp..n {
                 cof.a[i*n + j] -= r*cof.a[k*n + j];
             }
-            for j in 0..m+1 {
+            for j in 0..m {
                 cof.b[i*n + j] -= r*cof.b[k*n + j];
             }
         }
     }
 
-    for k in 0..m+1 {
+    for k in 0..m {
         cof.b[n*n + k] /= cof.a[n*n + n];
-        for i in (0..n).rev() {
+        for i in (0..n-1).rev() {
             let ip = i + 1;
-            for j in ip..n+1 {
+            for j in ip..n {
                 cof.b[i*n + k] -= cof.a[i*n + j] * cof.b[j*n + k];
             }
             cof.b[i*n + k] /= cof.a[i*n + i];
         }
     }
 
+}
+
+fn vpdis(sin_alpha: f64, cos_alpha: f64, bod: &mut BOD, cof: &mut COF) -> CPD {
+    let q = &cof.b[0..bod.ndtot];
+    let gamma = cof.b[cof.n-1];
+    let mut cp = vec![0.; bod.ndtot];
+    let mut ue = vec![0.; bod.ndtot];
+
+    println!("    j    X(j)      Y(j)      CP(j)      UE(j)");
+    for i in 0..bod.ndtot {
+        let mut v_tan = cos_alpha*bod.costhe[i] + sin_alpha*bod.sinthe[i];
+        for j in 0..bod.ndtot {
+            let (flog, ftan) = match j == i {
+                true => (0., std::f64::consts::PI),
+                false => {
+                    let dxj = bod.x_mid[i] - bod.x[j];
+                    let dxjp = bod.x_mid[i] - bod.x[j+1];
+                    let dyj = bod.y_mid[i] - bod.y[j];
+                    let dyjp = bod.y_mid[i] - bod.y[j+1];
+                    (0.5 * ((dxjp*dxjp + dyjp*dyjp)/(dxj*dxj + dyj*dyj)).ln(), (dyjp*dxj - dxjp*dyj).atan2(dxjp*dxj + dyjp*dyj))
+                },
+            };
+
+            let ctimtj = bod.costhe[i]*bod.costhe[j] + bod.sinthe[i]*bod.sinthe[j];
+            let stimtj  = bod.sinthe[i]*bod.costhe[j] - bod.costhe[i]*bod.sinthe[j];
+            let aa = 0.5/std::f64::consts::PI * (ftan * ctimtj + flog * stimtj);
+            let b = 0.5/std::f64::consts::PI * (flog * ctimtj - ftan * stimtj);
+            v_tan -= b*q[j] + gamma*aa;
+        }
+        cp[i]   = 1. - v_tan*v_tan;
+        ue[i]   = v_tan;
+        println!("{index} {xmid} {ymid} {cp} {ue}",
+                index = i,
+                xmid = bod.x_mid[i],
+                ymid = bod.y_mid[i],
+                cp = cp[i],
+                ue = ue[i]);
+    }
+
+    CPD {
+        ue, 
+        cp
+    }
 }
