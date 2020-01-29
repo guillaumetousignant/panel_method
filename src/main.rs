7877b6f57f74158aaafdb451b901a88e6a93eb06
Guillaume Tousignant <moi_guillaume@hotmail.com>
2020-01-29 10:31:29 -0500

    ted new function

--- src/main.rs
+++ src/main.rs
@@ -2,33 +2,46 @@ fn main() {
     println!("Hello, world!");
 }
 
+struct BOD {
+    ndtot: usize,
+    x: Vec<f64>,
+    y: Vec<f64>,
+    x_mid: Vec<f64>,
+    y_mid: Vec<f64>,
+    costhe: Vec<f64>,
+    sinthe: Vec<f64>,
+}
+
 struct COF {
     a: Vec<f64>,
     b: Vec<f64>,
     n: usize,
 }
 
+fn coef(sin_alpha: f64, cos_alpha: f64, bod: &mut BOD, cof: &mut COF) {
+
+}   
+
 // Assumes a is of size n*n and b is of size n
 fn gauss(m: usize, cof: &mut COF) {
-    println!("The value of m is: {}", m);
     let n = cof.b.len();
 
-    for k in 1..n {
+    for k in 0..n {
         let kp = k + 1;
         for i in kp..n+1 {
             let r = cof.a[i*n + k]/cof.a[k*n + k];
             for j in kp..n+1 {
                 cof.a[i*n + j] -= r*cof.a[k*n + j];
             }
-            for j in 1..m+1 {
+            for j in 0..m+1 {
                 cof.b[i*n + j] -= r*cof.b[k*n + j];
             }
         }
     }
 
-    for k in 1..m+1 {
+    for k in 0..m+1 {
         cof.b[n*n + k] /= cof.a[n*n + n];
-        for i in (n-1..0).rev() {
+        for i in (0..n).rev() {
             let ip = i + 1;
             for j in ip..n+1 {
                 cof.b[i*n + k] -= cof.a[i*n + j] * cof.b[j*n + k];
